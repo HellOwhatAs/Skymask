48948b030e075e14bec52032c8afbed4c5c7b0c3
HellOwhatAs <xjq701229@outlook.com>
2025-01-19 15:00:52 +0800

    忽略二维投影直线经过原点的线段

--- README.typ
+++ README.typ
@@ -3,22 +3,20 @@
 #let atan2 = math.op("atan2", limits: true)
 
 = 筛选问题
-对于二位平面上的点$(x, y)$，其与$x$轴正方向夹角为 $atan2(y, x)$，取值范围是$(-pi, pi]$
+对于二维度平面上的点$(x, y)$，其与$x$轴正方向夹角为 $atan2(y, x)$，取值范围是$(-pi, pi]$
 
 对于一条线段，记其两端点分别为$a=(x_a, y_a, z_a), b=(x_b, y_b, z_b)$。
-其夹角分别为$ theta_a = atan2(y_a, x_a), quad theta_b = atan2(y_b, x_b) $
 
-对$theta_a, theta_b$以及输入$theta$做一步变换$ 
-theta_((dot)) = cases(
-  2 pi + theta_((dot)) quad&"if" theta_((dot)) < 0, 
-  theta_((dot)) quad&"otherwise"
-) $
-这样$theta_a, theta_b$的取值范围就变成$[0, 2 pi)$了。不妨假设$theta_a < theta_b$，给定输入$theta in [0, pi)$，再做以下变换 $ (theta_a, theta_b, theta) = cases(
+如果$x_b y_a = x_a y_b$（在二维平面上的投影直线经过原点）或者#text(fill: blue)[超出距离]就扔掉这个线段
+
+$a, b$夹角分别为$ theta_a = atan2(y_a, x_a), quad theta_b = atan2(y_b, x_b) $
+$theta_a, theta_b$的取值范围是$[-pi, pi]$。
+不妨假设$theta_a < theta_b$，给定输入$theta in [0, pi)$，再做以下变换 $ (theta_a, theta_b, theta) = cases(
   (0, theta_b - theta_a, theta-theta_a) quad&"if" theta_b - theta_a < pi,
   (0, 2 pi + theta_a - theta_b, theta-theta_b) quad&"if" theta_b - theta_a > pi,
-  (0, 0, 0) quad&"if" theta_b - theta_a = pi quad "// 不太合理",
-) \
-theta = "fmod"("fmod"(theta, pi) + pi, pi) quad "// 属于" [0, pi) $
+) $
+此时$theta_a=0$，$theta_b in [0, pi)$。再对$theta$做以下变换：
+$ theta = "fmod"("fmod"(theta, pi) + pi, pi) quad "// 属于" [0, pi) $
 
 筛选满足条件 $theta_a <= theta <= theta_b$ 的线段
 
@@ -37,13 +35,3 @@ l = ((x_b y_a - x_a y_b))/k $
 如果算出来是负数，说明是直线在相反方向，这样就可以将问题输入范围简化为$[0, pi)$
 
 对于输入$theta in [0, pi)$，如果结果为正，则为 $theta$ 的值，而如果结果为负，则为负的$theta - pi$处的值
-
-特殊情况：$(x_a, y_a)$、$(x_b, y_b)$与原点共线
-
-记 $hat(alpha) = arctan(max(z_a/sqrt(x_a^2+y_a^2), z_b/sqrt(x_b^2+y_b^2)))$
-+ 如果$atan2(y_a, x_a) = atan2(y_b, x_b) = theta $则
-  $ alpha_(theta) = hat(alpha) $
-+ 如果$atan2(y_a, x_a) = atan2(y_b, x_b) = theta - pi $则
-  $ alpha_(theta-pi) = hat(alpha) $
-+ 如果$atan2(y_a, x_a) = -atan2(y_b, x_b) $，这种情况意味着坐标原点在建筑物边缘上，不太合理
-  // $ alpha_(theta) = alpha_(theta-pi) = pi/2 $
--- skymask.py
+++ skymask.py
@@ -1,8 +1,13 @@
 import numpy as np
 # import cupy as np
 from numpy.typing import NDArray
+from typing import Tuple
 
-def select_lines(theta: NDArray[np.float64], lines: NDArray[np.float64]):
+def select_lines(
+        theta: NDArray[np.float64],
+        lines: NDArray[np.float64], 
+        max_dist = np.inf
+    ) -> Tuple[NDArray[np.float64], NDArray[np.bool_]]:
     """
     Select lines based on the given angles.
     # Parameters:
@@ -11,40 +16,44 @@ def select_lines(theta: NDArray[np.float64], lines: NDArray[np.float64]):
     - lines: array with shape (n, 6) where each row represents  
       a line defined by two points (x1, y1, z1, x2, y2, z2).
     # Returns:
-    - np.ndarray: boolean array with shape (n, m) indicating whether  
+    - selected_lines: array with shape (n_, 6) where each row is from `lines`.
+    - selected_mask: boolean array with shape (n_, m) indicating whether  
       each angle in `theta` falls within the angle range of each line.
     """
+    lines = lines[
+        ((lines[:, 3] * lines[:, 1]) != (lines[:, 0] * lines[:, 4])) & 
+        (np.minimum(
+            np.linalg.norm(lines[:, 0:2], axis=1),
+            np.linalg.norm(lines[:, 3:5], axis=1)
+        ) < max_dist)
+    ]
+
     theta_a = np.arctan2(lines[:, 1], lines[:, 0])
     theta_b = np.arctan2(lines[:, 4], lines[:, 3])
 
     mask = theta_a > theta_b
     theta_a[mask], theta_b[mask] = theta_b[mask], theta_a[mask]
 
-    delta = theta_b - theta_a
-    mask1, mask2, mask3 = delta < np.pi, delta > np.pi, delta == np.pi
-
+    mask = (theta_b - theta_a) < np.pi
     theta = np.expand_dims(theta, axis=0).repeat(lines.shape[0], axis=0)
 
-    theta[mask1, :] -= theta_a[mask1, np.newaxis]
-    theta_b[mask1] -= theta_a[mask1]
-    theta_a[mask1] = 0
+    theta[mask, :] -= theta_a[mask, np.newaxis]
+    theta_b[mask] -= theta_a[mask]
 
-    theta[mask2, :] -= theta_b[mask2, np.newaxis]
-    theta_b[mask2] = 2 * np.pi + theta_a[mask2] - theta_b[mask2]
-    theta_a[mask2] = 0
-
-    theta[mask3] = 0
-    theta_b[mask3] = 0
-    theta_a[mask3] = 0
+    mask = ~mask
+    theta[mask, :] -= theta_b[mask, np.newaxis]
+    theta_b[mask] = 2 * np.pi + theta_a[mask] - theta_b[mask]
 
     theta = np.fmod(np.fmod(theta, np.pi) + np.pi, np.pi)
 
-    return theta <= theta_b[:, np.newaxis]
+    return lines, theta <= theta_b[:, np.newaxis]
 
 
 def calc_alpha(
-    theta: NDArray[np.float64], lines: NDArray[np.float64], selected: NDArray[np.bool_]
-):
+    theta: NDArray[np.float64],
+    lines: NDArray[np.float64], 
+    selected: NDArray[np.bool_]
+) -> Tuple[NDArray[np.float64], NDArray[np.float64]]:
     """
     Calculate angle of elevation based on theta and selected lines.
     # Parameters:
@@ -68,8 +77,8 @@ def calc_alpha(
             (lines[:, 3] * lines[:, 2] - lines[:, 0] * lines[:, 5])
                 .reshape((-1, 1))
             @ np.sin(theta).reshape((1, -1))
-        ) / (lines[:, 3] * lines[:, 1] - lines[:, 0] * lines[:, 4])[:, np.newaxis]
-        )
+        ) / (lines[:, 3] * lines[:, 1] - lines[:, 0] * lines[:, 4])
+                .reshape(-1, 1))
     )
 
     alpha_1: NDArray[np.float64] = np.maximum(alphas.max(axis=0), 0)
@@ -88,14 +97,15 @@ if __name__ == "__main__":
 
     theta = np.linspace(0, np.pi, num=500, endpoint=False)
 
-    selected = select_lines(theta, lines)
+    selected_lines, selected = select_lines(theta, lines)
+    a1, a2 = calc_alpha(theta, selected_lines, selected)
 
-    a1, a2 = calc_alpha(theta, lines, selected)
-    
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
     ax.yaxis.set_major_formatter("")
-    ax.plot(theta, np.pi/2 - a1)
-    ax.plot((theta - np.pi), np.pi/2 - a2)
+    x = np.concatenate((theta, (theta - np.pi), [theta[0]]), axis=0)
+    y= np.concatenate((np.pi/2 - a1, np.pi/2 - a2, [np.pi/2 - a1[0]]), axis=0)
+    ax.plot(x, y)
+    ax.fill_between(x, y, np.pi/2, alpha=0.2)
     ax.set_ylim(0, np.pi / 2)
     plt.show()
